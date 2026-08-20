a8b17ab5e8e2ed7bb12f01ea7c5e44171979bd6a
Daniel Juan <dannijl88web@gmail.com>
2026-08-20 15:32:07 +0200

    Initial commit

---
+++ src/main/java/com/dani/tienda_musica/TiendaMusicaApplication.java
@@ -0,0 +1,13 @@
+package com.dani.tienda_musica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMusicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dani/tienda_musica/TiendaMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dani.tienda_musica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
